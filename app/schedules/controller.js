b1b233b03eef84f3d9a272eb52a43d7b9c7d1bd5
JOHNEPPILLAR <johneppillar@gmail.com>
2020-02-22 16:25:20 +0000

    refactored schedule setup

--- app/schedules/controller.js
+++ app/schedules/controller.js
@@ -10,32 +10,26 @@ const dateformat = require('dateformat');
  */
 const tpLink = require('./tp_link.js');
 
-// Set up the schedules
-async function setupSchedules() {
+async function setSchedule() {
   // Cancel any existing schedules
   serviceHelper.log(
     'trace',
     'Removing any existing schedules',
   );
   await global.schedules.map((value) => value.cancel());
-  await tpLink.setup(); // Set new schedules
-}
-
-exports.setSchedule = async () => {
-  await setupSchedules();
 
   // Set schedules each day to keep in sync with sunrise & sunset changes
   const date = new Date();
   date.setHours(3);
   date.setMinutes(5);
   date.setTime(date.getTime() + 1 * 86400000);
-  const schedule = scheduler.scheduleJob(date, () => {
-    serviceHelper.log('info', 'Resetting daily schedules to keep in sync with sunrise & sunset changes');
-    setupSchedules();
-  }); // Set the schedule
+  const schedule = scheduler.scheduleJob(date, () => setSchedule()); // Set the schedule
   global.schedules.push(schedule);
   serviceHelper.log(
     'info',
     `Reset schedules will run on ${dateformat(date, 'dd-mm-yyyy @ HH:MM')}`,
   );
-};
+  tpLink.setup();
+}
+
+exports.setSchedule = setSchedule;
