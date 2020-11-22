f5b6f2c7490cad12d546da8f7cfafa592a246a00
JP <johneppillar@gmail.com>
2020-11-22 20:19:35 +0000

    2.1.0

---
+++ healthcheck.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "Set env vars"
+URL=https://localhost:$PORT/ping
+
+echo "Call url"
+if [ $(curl -L --insecure $URL -o /dev/null -w '%{http_code}\n' -s) == "401" ]
+then exit 0
+else exit 1
+fi
